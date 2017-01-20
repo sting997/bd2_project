9d7b576798bdbd3c5c5fe9aace0f47e1a45bc0c8
Mike <sting997@gmail.com>
2017-01-20 19:56:20 +0100

    implemented adding new event

--- src/controller/EventTabController.java
+++ src/controller/EventTabController.java
@@ -1,12 +1,11 @@
 package controller;
 
-import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Table;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -14,7 +13,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import bd2.Adress;
 import bd2.Event;
 import bd2.EventType;
 import bd2.Stadium;
@@ -22,46 +20,32 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 public class EventTabController {
 	private SessionFactory factory;
 	@FXML
 	private TableView<Event> eventTableView;
 	@FXML
-	private TableColumn eventIdTableColumn;
+	private TableColumn<Event, Integer> eventIdTableColumn;
 	@FXML
-	private TableColumn eventNameTableColumn;
+	private TableColumn<Event, String> eventNameTableColumn;
 	@FXML
-	private TableColumn eventDateTableColumn;
+	private TableColumn<Event, Date> eventDateTableColumn;
 	@FXML
 	private TableColumn<Event, Stadium> eventStadiumTableColumn;
 	@FXML
-	private TableColumn eventTypeTableColumn;
+	private TableColumn<Event, EventType> eventTypeTableColumn;
 	@FXML
 	private Tab eventTab;
-	// @FXML
-	// private TextField idTextField;
-	// @FXML
-	// private TextField nameTextField;
-	// @FXML
-	// private TextField dateTextField;
-	// @FXML
-	// private TextField stadiumTextField;
-	// @FXML
-	// private TextField typeTextField;
 	@FXML
 	private Button addEventButton;
 	@FXML
@@ -78,18 +62,63 @@ public class EventTabController {
 		deleteEventButton.setOnAction((ActionEvent event) -> {
 			handleDelete();
 		});
+		editEventButton.setOnAction((ActionEvent event) -> {
+			// TODO handler
+		});
 	}
 
 	private void handleAdd() {
-		// TODO
-		Parent root;
-		// root =
-		// FXMLLoader.load(getClass().getClassLoader().getResource("path/to/other/view.fxml"),
-		// resources);
-		GridPane grid = new GridPane();
+		HBox root = new HBox();
+		TextField nameTextField = new TextField("Name");
+		TextField dateTextField = new TextField("Date");
+		TextField stadiumTextField = new TextField("Stadium id");
+		TextField typeTextField = new TextField("Type id");
+		Button addButton = new Button("Add");
+		root.getChildren().add(nameTextField);
+		root.getChildren().add(dateTextField);
+		root.getChildren().add(stadiumTextField);
+		root.getChildren().add(typeTextField);
+		root.getChildren().add(addButton);
+		
+		addButton.setOnAction((ActionEvent event) -> {
+			byte stadiumId = Byte.parseByte(stadiumTextField.getText());
+			byte typeId = Byte.parseByte(typeTextField.getText());
+			String name = nameTextField.getText();
+			DateFormat dateFormat = new SimpleDateFormat("yyy-MM-dd");
+			Date date;
+			try {
+				date = dateFormat.parse(dateTextField.getText());
+				Session session = factory.openSession();
+				Transaction tx = null;
+				try {
+					tx = session.beginTransaction();
+					Stadium stadium =(Stadium)session.get(Stadium.class, stadiumId);
+					EventType eventType = (EventType) session.get(EventType.class, typeId);
+					Event newEvent = new Event();
+					newEvent.setName(name);
+					newEvent.setDate(date);
+					newEvent.setStadium(stadium);
+					newEvent.setEventType(eventType);
+					Integer newEventId = (Integer) session.save(newEvent);
+					tx.commit();
+					eventTableView.getItems().add(newEvent);
+				} catch (HibernateException e) {
+					if (tx != null)
+						tx.rollback();
+					//TODO print some info
+					e.printStackTrace();
+				} finally {
+					session.close();
+				}
+			} catch (ParseException e) {
+				//TODO print some info
+				e.printStackTrace();
+			}
+		});
+		
 		Stage stage = new Stage();
-		stage.setTitle("My New Stage Title");
-		stage.setScene(new Scene(grid, 450, 450));
+		stage.setTitle("Add Event");
+		stage.setScene(new Scene(root));
 		stage.show();
 
 	}
@@ -108,7 +137,7 @@ public class EventTabController {
 			} catch (HibernateException e) {
 				if (tx != null)
 					tx.rollback();
-				//TODO jakos na ekranie pieknie pokazac info ze sie nie da 
+				// TODO jakos na ekranie pieknie pokazac info ze sie nie da
 				e.printStackTrace();
 			} finally {
 				session.close();
@@ -117,12 +146,11 @@ public class EventTabController {
 	}
 
 	private void loadData() {
-		eventIdTableColumn.setCellValueFactory(new PropertyValueFactory<Event, String>("id"));
+		eventIdTableColumn.setCellValueFactory(new PropertyValueFactory<Event, Integer>("id"));
 		eventNameTableColumn.setCellValueFactory(new PropertyValueFactory<Event, String>("name"));
 		eventDateTableColumn.setCellValueFactory(new PropertyValueFactory<Event, Date>("date"));
 		eventStadiumTableColumn.setCellValueFactory(new PropertyValueFactory<Event, Stadium>("stadium"));
-		eventTypeTableColumn.setCellValueFactory(new PropertyValueFactory<Event, String>("eventType"));
-		;
+		eventTypeTableColumn.setCellValueFactory(new PropertyValueFactory<Event, EventType>("eventType"));
 
 		try {
 			factory = new Configuration().configure("/resources/hibernate.cfg.xml").buildSessionFactory();
@@ -146,7 +174,8 @@ public class EventTabController {
 		} finally {
 			session.close();
 		}
-
+		//TODO factory needs to be closed at the end of programme
+		//otherwise the programme does not stop and needs to be terminated manually
 		// factory.close();
 	}
 
